274c7a2147db6fbd367dc005924466b19a90ad56
Weinan Shi <shi.wein@northeastern.edu>
2023-04-15 23:44:30 -0700

    somedoc

--- src/main/java/ReservationSystem.java
+++ src/main/java/ReservationSystem.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * show 功能正常 但是不美观 10排以后和10排之前的不是对齐的
+ * reserve 功能不正常 reserve的时候 轮椅或者不是轮椅都是从第一排开始预定
+ * done 功能正常
+ */
 public class ReservationSystem {
     public static void main(String[] args) {
         Theater theater = new Theater("NEU", 15, 10, new ArrayList<>(Arrays.asList(2, 10)));
--- src/main/java/Seat.java
+++ src/main/java/Seat.java
@@ -1,24 +1,51 @@
+/**
+ * The Seat class represents a single seat in a theater.
+ * It stores information about the seat's name and reservation status.
+ * The name of the seat.
+ * The name of the person the seat is reserved for, or null if the seat is not reserved.
+ */
 public class Seat {
     private String name;
     private String reservedFor;
 
+    /**
+     * Constructs a Seat object with the given name.
+     * @param name the name of the seat.
+     */
     public Seat(String name) {
         this.name = name;
         this.reservedFor = null;
     }
 
+    /**
+     * Returns the name of the seat.
+     * @return  the name of the seat.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the name of the person the seat is reserved for, or null if the seat is not reserved.
+     * @return the name of the person the seat is reserved for, or null if not reserved.
+     *
+     */
     public String getReservedFor() {
         return reservedFor;
     }
 
+    /**
+     * Sets the name of the person the seat is reserved for.
+     * @param reservedFor reservedFor the name of the person the seat is reserved for.
+     */
     public void setReservedFor(String reservedFor) {
         this.reservedFor = reservedFor;
     }
 
+    /**
+     * Checks if the seat is reserved.
+     * @return true if the seat is reserved, false otherwise
+     */
     public boolean isReserved() {
         return reservedFor != null;
     }
